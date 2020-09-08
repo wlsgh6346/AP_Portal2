f96806a233de17ff053882ca900cdae6bbfd6a68
wlsgh6346 <wlsgh6247@gmail.com>
2020-09-08 13:39:00 +0900

    crew portal css / js 변경 사항

--- hyper/dist/creative/assets/js/pages/demo.datatable-init.js
+++ hyper/dist/creative/assets/js/pages/demo.datatable-init.js
@@ -1,1 +1,108 @@
-$(document).ready(function(){"use strict";$("#basic-datatable").DataTable({keys:!0,language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}});var a=$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","print"],language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}});$("#selection-datatable").DataTable({select:{style:"multi"},language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}}),a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),$("#alternative-page-datatable").DataTable({pagingType:"full_numbers",drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}}),$("#scroll-vertical-datatable").DataTable({scrollY:"350px",scrollCollapse:!0,paging:!1,language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}}),$("#scroll-horizontal-datatable").DataTable({scrollX:!0,language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}}),$("#complex-header-datatable").DataTable({language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")},columnDefs:[{visible:!1,targets:-1}]}),$("#row-callback-datatable").DataTable({language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")},createdRow:function(a,i,e){15e4<1*i[5].replace(/[\$,]/g,"")&&$("td",a).eq(5).addClass("text-danger")}}),$("#state-saving-datatable").DataTable({stateSave:!0,language:{paginate:{previous:"<i class='mdi mdi-chevron-left'>",next:"<i class='mdi mdi-chevron-right'>"}},drawCallback:function(){$(".dataTables_paginate > .pagination").addClass("pagination-rounded")}})});
+$(document).ready(function () {
+    "use strict";
+    $("#basic-datatable").DataTable({
+        keys: !0,
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            }
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        }
+    });
+    var a = $("#datatable-buttons").DataTable({
+        lengthChange: !1,
+        buttons: ["copy", "print"],
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            }
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        }
+    });
+    $("#selection-datatable").DataTable({
+        select: {
+            style: "multi"
+        },
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            }
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        }
+    }), a.buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $("#alternative-page-datatable").DataTable({
+        pagingType: "full_numbers",
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        }
+    }), $("#scroll-vertical-datatable").DataTable({
+        scrollY: "350px",
+        scrollCollapse: !0,
+        paging: !1,
+        language: {
+            paginate: {
+                previous: "<i class='mdi mdi-chevron-left'>",
+                next: "<i class='mdi mdi-chevron-right'>"
+            }
+        },
+        drawCallback: function () {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
+        }
+    }), $("#scroll-horizontal-datatable").DataTable({
+        scrollX: true,
+        dom: 'Bfrtip',
+        buttons: [
+            'excel'
+        ],
+        info: false,
+        sort: false,
+        filter: false,
+        lengthChange: false,
+        drawCallback: function () {
+            $("#scroll-horizontal-datatable_paginate > .pagination").addClass("mt-3 mb-3");
+            $("#scroll-horizontal-datatable_paginate > .pagination").css('justify-content', 'center');
+            $(".dt-buttons").addClass("float-right");
+            $(".buttons-excel").removeClass("btn-secondary");
+            $(".buttons-excel").addClass("btn-success");
+            $(".buttons-excel").html('<i class="mdi mdi-file-excel-outline"></i> EXECL');
+        },
+    }),$("#scroll-horizontal-expiry").DataTable({
+        scrollX: !0,
+        info: false,
+        sort: false,
+        filter: false,
+        lengthChange: false,
+        drawCallback: function () {
+            $("#scroll-horizontal-expiry_paginate > .pagination").addClass("mt-3 mb-3");
+            $("#scroll-horizontal-expiry_paginate > .pagination").css('justify-content', 'left');
+        },
+    }),$("#route-datatable").DataTable({
+        scrollX: !0,
+        scrollY: '300px',
+        info: false,
+        sort: false,
+        filter: false,
+        paging: false,
+        lengthChange: false,
+        drawCallback: function () {
+        },
+    }),$("#airport-datatable").DataTable({
+        scrollX: !0,
+        scrollY: '300px',
+        info: false,
+        sort: false,
+        filter: false,
+        paging: false,
+        lengthChange: false,
+        drawCallback: function () {
+        },
+    })
+});
